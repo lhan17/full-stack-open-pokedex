ebd5fd08a1b05c240f997a705fa8fffc1b59fde6
lhan17 <lam.dthinh@gmail.com>
2023-04-30 11:04:03 +0300

    11.8

--- cypress/e2e/pokedex.cy.js
+++ cypress/e2e/pokedex.cy.js
@@ -5,10 +5,10 @@ describe('Pokedex', function () {
         cy.contains(
             'Pokémon and Pokémon character names are trademarks of Nintendo.'
         )
-    }),
-        it('pokemon page can be navigated to', function () {
-            cy.visit('http://localhost:8080')
-            cy.contains('ivysaur').click()
-            cy.contains('chlorophyll')
-        })
+    })
+    it('pokemon page can be navigated to', function () {
+        cy.visit('http://localhost:8080')
+        cy.contains('ivysaur').click()
+        cy.contains('chlorophyll')
+    })
 })
